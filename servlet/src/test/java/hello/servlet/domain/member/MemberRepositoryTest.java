85630e2b7819f300acbcf44e90f568d29842fc1a
cjsxorud111 <cjsxorud111@gmail.com>
2023-06-07 19:16:12 +0900

    findAll테스트 추가

--- servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
+++ servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -5,17 +5,16 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.*;
 
-
 class MemberRepositoryTest {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
-
-    private static Long sequence = 0L;
 
     @AfterEach
     void afterEach() {
@@ -31,17 +30,31 @@ class MemberRepositoryTest {
 
         //when
 
-        memberRepository.save(member);
+        Member m = memberRepository.save(member);
 
         //then
-        assertThat(memberRepository.findById(1L).getUsername()).isEqualTo("지섭");
+        assertThat(memberRepository.findById(0L).getUsername()).isEqualTo("지섭");
     }
 
     @Test
     void findAll() {
+        //given
         Member member = new Member();
         member.setAge(21);
         member.setUsername("지섭");
+        memberRepository.save(member);
+        Member member1 = new Member();
+        member1.setAge(23);
+        member1.setUsername("재형");
+        memberRepository.save(member1);
+
+        //when
+        List<Member> members = memberRepository.findAll();
+
+        //then
+        assertThat(members.get(0).getUsername()).isEqualTo("지섭");
+        assertThat(members.get(1).getUsername()).isEqualTo("재형");
+
     }
 
 }
